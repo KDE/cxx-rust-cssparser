c55cc9766d9ad196fbe563181c19a461ac5e4368
Arjen Hiemstra <ahiemstra@heimr.nl>
2025-05-14 12:30:44 +0200

    Convert ParseError from an enum into a struct with fields
    So that we can include source location information in the error.

--- rust/src/details/mod.rs
+++ rust/src/details/mod.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 // SPDX-FileCopyrightText: 2025 Arjen Hiemstra <ahiemstra@heimr.nl>
 
-use std::io;
-
 pub mod identifier;
 pub mod rulesparser;
 pub mod selectorparser;
@@ -10,24 +8,47 @@ pub mod selectorparser;
 pub mod propertysyntax;
 pub mod propertyparser;
 
-#[derive(Debug)]
-pub enum ParseError {
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParseErrorKind {
     Unspecified,
-    Unknown(String),
-    UnknownProperty(String),
-    InvalidSelectors(String),
-    InvalidPropertySyntax(String),
-    InvalidPropertyValue(String),
-    InvalidPropertyDefinition(String),
-    UnsupportedAtRule(String),
-    FileError(io::Error),
+    UnexpectedEndOfInput,
+    Unknown,
+    UnknownProperty,
+    InvalidSelectors,
+    InvalidPropertySyntax,
+    InvalidPropertyValue,
+    InvalidPropertyDefinition,
+    UnsupportedAtRule,
+    FileError,
 }
 
-#[macro_export]
-macro_rules! parse_error {
-    ($p:ident, $i:ident, $m:expr) => {
-        Err($p.new_custom_error(ParseError::$i($m)))
-    };
+#[derive(Debug, Clone)]
+pub struct SourceLocation {
+    pub file: String,
+    pub line: u32,
+    pub column: u32,
+}
+
+impl SourceLocation {
+    pub fn from_file_location(file: String, location: cssparser::SourceLocation) -> SourceLocation {
+        SourceLocation { file, line: location.line, column: location.column }
+    }
+
+    pub fn from_file(file: &str) -> SourceLocation {
+        SourceLocation { file: file.to_string(), line: 0, column: 0 }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ParseError {
+    pub kind: ParseErrorKind,
+    pub message: String,
+    pub location: SourceLocation,
+}
+
+
+pub fn parse_error<'i, 't, R>(parser: &cssparser::Parser<'i, 't>, kind: ParseErrorKind, message: String) -> Result<R, cssparser::ParseError<'i, ParseError>> {
+    Err(parser.new_custom_error(ParseError{ kind, message, location: SourceLocation::from_file_location(parser.current_source_url().unwrap_or("").to_string(), parser.current_source_location())}))
 }
 
 impl std::error::Error for ParseError {
@@ -35,38 +56,59 @@ impl std::error::Error for ParseError {
 
 impl std::fmt::Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.kind {
+            ParseErrorKind::Unspecified => write!(f, "Unspecified error"),
+            ParseErrorKind::Unknown => write!(f, "Unknown error: {}", self.message),
+            ParseErrorKind::UnknownProperty => write!(f, "Unknown Property: {}", self.message),
+            ParseErrorKind::InvalidSelectors => write!(f, "Invalid Selectors: {}", self.message),
+            ParseErrorKind::InvalidPropertySyntax => write!(f, "Invalid property syntax: {}", self.message),
+            ParseErrorKind::InvalidPropertyValue => write!(f, "Invalid property value: {}", self.message),
+            ParseErrorKind::InvalidPropertyDefinition => write!(f, "Invalid property definition: {}", self.message),
+            ParseErrorKind::UnsupportedAtRule => write!(f, "Unsupported @-rule: {}", self.message),
+            ParseErrorKind::FileError => write!(f, "IO Error: {}", self.message),
+        }
+    }
+}
+
+impl ParseError {
+    fn from_cssparser_error<'i, E>(value: cssparser::ParseError<'i, E>, file: String) -> Self
+        where E: ToParseError,
+    {
+        match value.kind {
+            cssparser::ParseErrorKind::Basic(basic_error) => basic_error.to_parse_error(file, value.location),
+            cssparser::ParseErrorKind::Custom(custom_error) => custom_error.to_parse_error(file, value.location),
+        }
+    }
+}
+
+trait ToParseError {
+    fn to_parse_error(&self, file: String, location: cssparser::SourceLocation) -> ParseError;
+}
+
+impl ToParseError for cssparser::BasicParseErrorKind<'_> {
+    fn to_parse_error(&self, file: String, location: cssparser::SourceLocation) -> ParseError {
+        let location = SourceLocation::from_file_location(file, location);
         match self {
-            ParseError::Unspecified => write!(f, "{}", "Unspecified error"),
-            ParseError::Unknown(message) => write!(f, "Unknown error: {}", message),
-            ParseError::UnknownProperty(message) => write!(f, "Unknown Property: {}", message),
-            ParseError::InvalidSelectors(message) => write!(f, "Invalid Selectors: {}", message),
-            ParseError::InvalidPropertySyntax(message) => write!(f, "Invalid property syntax: {}", message),
-            ParseError::InvalidPropertyValue(message) => write!(f, "Invalid property value: {}", message),
-            ParseError::InvalidPropertyDefinition(message) => write!(f, "Invalid property definition: {}", message),
-            ParseError::UnsupportedAtRule(message) => write!(f, "Unsupported @-rule: {}", message),
-            ParseError::FileError(error) => write!(f, "{}", error),
+            cssparser::BasicParseErrorKind::UnexpectedToken(token) => ParseError{ kind: ParseErrorKind::UnexpectedToken, message: format!("{:?}", token), location },
+            cssparser::BasicParseErrorKind::EndOfInput => ParseError{ kind: ParseErrorKind::UnexpectedEndOfInput, message: String::new(), location },
+            cssparser::BasicParseErrorKind::AtRuleInvalid(at_rule) => ParseError{ kind: ParseErrorKind::InvalidAtRule, message: at_rule.to_string(), location },
+            cssparser::BasicParseErrorKind::AtRuleBodyInvalid => ParseError{ kind: ParseErrorKind::InvalidAtRule, message: String::from("Invalid @-rule body"), location },
+            cssparser::BasicParseErrorKind::QualifiedRuleInvalid => ParseError{ kind:ParseErrorKind::InvalidQualifiedRule, message: String::new(), location },
         }
     }
 }
 
-impl From<selectors::parser::SelectorParseErrorKind<'_>> for ParseError {
-    fn from(value: selectors::parser::SelectorParseErrorKind<'_>) -> Self {
-        match value {
-            selectors::parser::SelectorParseErrorKind::NoQualifiedNameInAttributeSelector(_) => ParseError::InvalidSelectors(String::from("No qualified name in attribute selector")),
-            selectors::parser::SelectorParseErrorKind::EmptySelector => ParseError::InvalidSelectors(String::from("Empty Selector")),
-            selectors::parser::SelectorParseErrorKind::DanglingCombinator => ParseError::InvalidSelectors(String::from("Dangling Combinator")),
-            selectors::parser::SelectorParseErrorKind::NonCompoundSelector => ParseError::InvalidSelectors(String::from("Non-compound Selector")),
-            _ => ParseError::InvalidSelectors(String::from("Selectors failed to parse")),
-        }
-    }
-}
-
-impl From<cssparser::ParseError<'_, selectors::parser::SelectorParseErrorKind<'_>>> for ParseError {
-    fn from(value: cssparser::ParseError<'_, selectors::parser::SelectorParseErrorKind<'_>>) -> Self {
-        if let cssparser::ParseErrorKind::Custom(selector_error) = value.kind {
-            ParseError::from(selector_error)
-        } else {
-            ParseError::Unknown(String::from("Unknown parse error"))
+impl ToParseError for selectors::parser::SelectorParseErrorKind<'_>
+{
+    fn to_parse_error(&self, file: String, location: cssparser::SourceLocation) -> ParseError {
+        let location = SourceLocation::from_file_location(file, location);
+        match self {
+            selectors::parser::SelectorParseErrorKind::NoQualifiedNameInAttributeSelector(_) =>
+                ParseError{ kind: ParseErrorKind::InvalidSelectors, message: String::from("No qualified name in attribute selector"), location },
+            selectors::parser::SelectorParseErrorKind::EmptySelector => ParseError{ kind: ParseErrorKind::InvalidSelectors, message: String::from("Empty Selector"), location },
+            selectors::parser::SelectorParseErrorKind::DanglingCombinator => ParseError{ kind: ParseErrorKind::InvalidSelectors, message: String::from("Dangling Combinator"), location },
+            selectors::parser::SelectorParseErrorKind::NonCompoundSelector => ParseError{ kind: ParseErrorKind::InvalidSelectors, message: String::from("Non-compound Selector"), location },
+            _ => ParseError{ kind: ParseErrorKind::InvalidSelectors, message: String::from("Selectors failed to parse"), location },
         }
     }
 }
