56ec33ff52766ea97b6975596c01c431a998f8b1
Arjen Hiemstra <ahiemstra@heimr.nl>
2025-08-21 18:13:02 +0200

    example: Add option parsing and options to prepend/append extra stylesheets
    This makes it a more useful tool to actually inspect whether a file
    correcltly parses by allowing specifying dependent CSS files on the
    command line.

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <iostream>
 
+#include <getopt.h>
+
 #include "CssParser.h"
 
 using namespace cssparser;
@@ -53,25 +55,85 @@ std::string value_to_string(const Value &value)
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
+    std::vector<std::filesystem::path> prepend_files;
+    std::vector<std::filesystem::path> append_files;
+
+    int show_help = 0;
+
+    auto options = std::array{
+        option { .name = "prepend", .has_arg = required_argument, .flag = nullptr, .val = 'p' },
+        option { .name = "append", .has_arg = required_argument, .flag = nullptr, .val = 'a' },
+        option { .name = "help", .has_arg = no_argument, .flag = nullptr, .val = 'h' },
+    };
+
+    int c = 0;
+    int i = 0;
+    while ((c = getopt_long(argc, argv, "", options.data(), &i)) != -1) {
+        switch (c) {
+        case 'p':
+            prepend_files.push_back(std::string(optarg));
+            break;
+        case 'a':
+            append_files.push_back(std::string(optarg));
+            break;
+        case 'h':
+            show_help = 1;
+            break;
+        default:
+            std::cout << "Unrecognized option: " << c;
+            show_help = 1;
+        }
+    }
+
+    if (optind >= argc && show_help == 0) {
         std::cerr << "A file path is required!" << std::endl;
+        show_help = 1;
+    }
+
+    if (show_help != 0) {
+        std::cout << "Usage: " << argv[0] << "[options] <filename>\n\n";
+        std::cout << "Options:\n";
+        std::cout << "--prepend <filename> Add and parse <filename> before parsing the main file.\n";
+        std::cout << "--append <filename> Add and parse <filename> after parsing the main file.\n";
         exit(1);
     }
 
-    std::filesystem::path path(argv[1]);
+    std::filesystem::path path(argv[optind]);
 
     StyleSheet sheet;
+
+    for (auto file : prepend_files) {
+        if (file.has_parent_path()) {
+            sheet.set_root_path(file.parent_path());
+            sheet.parse_file(file.filename());
+        } else{
+            sheet.set_root_path(path.parent_path());
+            sheet.parse_file(file);
+        }
+    }
+
     sheet.set_root_path(path.parent_path());
     sheet.parse_file(path.filename());
+
+    for (auto file : append_files) {
+        if (file.has_parent_path()) {
+            sheet.set_root_path(file.parent_path());
+            sheet.parse_file(file.filename());
+        } else{
+            sheet.set_root_path(path.parent_path());
+            sheet.parse_file(file);
+        }
+    }
 
     auto errors = sheet.errors();
     if (!errors.empty()) {
         std::cout << errors.size() << " errors:" << std::endl;
 
         for (auto error : errors) {
-            std::cout << "In file " << error.file << " on line " << error.line << " column " << error.column << ":" << std::endl;
             std::cout << error.message << std::endl;
         }
+
+        exit(2);
     }
 
     auto result = sheet.rules();
