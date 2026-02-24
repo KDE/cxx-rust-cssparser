a5c0af82ffd6245fa4213fdefff471b519b71765
Arjen Hiemstra <ahiemstra@heimr.nl>
2026-02-24 12:38:53 +0100

    Remove obsolete build file
    It's now all handled by Corrosion

--- rust/build.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BSD-2-Clause
-// SPDX-FileCopyrightText: 2025 Arjen Hiemstra <ahiemstra@heimr.nl>
-
-fn main() {
-    let _ = cxx_build::bridge("src/ffi.rs");
-
-    println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=src/ffi.rs");
-}
