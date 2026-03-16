da0942fb954e520eda42bcfb252df9cedf095101
Arjen Hiemstra <ahiemstra@heimr.nl>
2026-03-16 18:16:39 +0100

    Add support for generating QDoc based documentation for the C++ library
    Along with some initial class documentation.

--- cpp/CssParser.h
+++ cpp/CssParser.h
@@ -17,24 +17,46 @@ struct Property;
 struct StyleRule;
 }
 
+/*!
+ * \class cssparser::Property
+ * \inmodule cxx-rust-cssparser
+ *
+ * \brief A property of a CSS Rule.
+ */
 class CSSPARSER_EXPORT Property
 {
 public:
+    /*!
+     * Constructor.
+     *
+     * Constructs a new Property with  \a name as name and \a values as values.
+     */
     Property(const std::string &name, const std::vector<Value> &values);
+    /*!
+     * Returns the name of this Property.
+     */
     inline std::string name() const
     {
         return m_name;
     }
+    /*!
+     * Returns a view on the values of this Property.
+     */
     inline std::span<const Value> values() const
     {
         return std::span<const Value>(m_values.cbegin(), m_values.cend());
     }
-    template <typename T>
+    /*!
+     * Returns the value at \a index as type T.
+     */
+    template<typename T>
     inline T value(std::size_t index = 0) const
     {
         return m_values.at(index).get<T>();
     }
-
+    /*!
+     * Returns the value at \a index.
+     */
     inline Value value(std::size_t index = 0) const
     {
         return m_values.at(index);
@@ -48,15 +70,35 @@ private:
     std::vector<Value> m_values;
 };
 
+/*!
+ * \class cssparser::Rule
+ * \inmodule cxx-rust-cssparser
+ *
+ * \brief A single styling rule.
+ */
 class CSSPARSER_EXPORT Rule
 {
 public:
+    /*!
+     * Default constructor.
+     */
     Rule();
+    /*!
+     * Constructor.
+     *
+     * Constructs a Rule with \a selector as selector and \a properties as properties.
+     */
     Rule(const Selector &selector, const std::vector<Property> &properties);
+    /*!
+     * Returns the selector of this Rule.
+     */
     inline Selector selector() const
     {
         return m_selector;
     }
+    /*!
+     * Returns the properties associated with this Rule.
+     */
     inline std::span<const Property> properties() const
     {
         return std::span<const Property>(m_properties.cbegin(), m_properties.cend());
@@ -70,6 +112,11 @@ private:
     std::vector<Property> m_properties;
 };
 
+/*!
+ * \inmodule cxx-rust-cssparser
+ *
+ * \brief A struct describing an error that happend during parsing.
+ */
 struct CSSPARSER_EXPORT Error {
     std::string file;
     uint32_t line = 0;
@@ -77,16 +124,59 @@ struct CSSPARSER_EXPORT Error {
     std::string message;
 };
 
+/*!
+ * \inmodule cxx-rust-cssparser
+ *
+ * \brief A collection of style rules.
+ *
+ */
 class CSSPARSER_EXPORT StyleSheet
 {
 public:
+    /*!
+     * Default constructor.
+     */
     StyleSheet();
     ~StyleSheet();
 
+    /*!
+     * A view of the list of rules contained in this StyleSheet.
+     */
     std::span<const Rule> rules() const;
+    /*!
+     * A view of the list of errors generated when parsing this StyleSheet.
+     */
     std::span<const Error> errors() const;
+    /*!
+     * Set the root path of this StyleSheet to \a path.
+     *
+     * The root path determines relative to what path relative imports are
+     * resolved by. An import like \c{@import "example.css";}
+     */
     void setRootPath(const std::filesystem::path &path);
+    /*!
+     * Parse a CSS file and add all rules to this StyleSheet.
+     *
+     * This will read the file from \a file_name and parse it. After this, all
+     * sucessfully parsed rules are available through rules(). Any errors
+     * encountered while parsing will be available through errors().
+     *
+     * \note Multiple calls will append to the internal list of rules and
+     * errors.
+     */
     void parseFile(const std::string &file_name);
+    /*!
+     * Parse a string containing CSS and add all rules to this StyleSheet.
+     *
+     * This will parse the CSS in \a data. After this, all sucessfully parsed
+     * rules are available through rules(). Any errors encountered while parsing
+     * will be available through errors().
+     *
+     * \a origin is used as the file path that errors are reported from.
+     *
+     * \note Multiple calls will append to the internal list of rules and
+     * errors.
+     */
     void parseString(const std::string &data, const std::string &origin);
 
 private:
